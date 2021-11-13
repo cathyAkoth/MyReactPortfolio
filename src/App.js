2d6ee7f56c8ec3f2ece93ed905985520acdfb436
CathyAkoth <catherinececilia22@gmail.com>
2021-11-13 02:38:30 -0800

    AddedHomePage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+function App(){
+  return(
+    <div className="App">
+      <div className="sidebar"></div>
+      <div className="main-content"></div>
+    </div>
+  );
 
+}
+
+export default App;
 
 
 
